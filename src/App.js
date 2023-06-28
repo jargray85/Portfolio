f264bec8e8cbd2737375f745039f08faedefbd76
James Gray <jamesrjgray@gmail.com>
2023-06-28 17:09:55 -0500

    changed url to deployed heroku app for deployment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Projects from "./pages/Projects";
 
 function App() {
   // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
-  const URL = "http://localhost:4000/"
+  const URL = "https://james-gray-portfolio-e06bd198346a.herokuapp.com/"
 
   return (
     <div className="App">
